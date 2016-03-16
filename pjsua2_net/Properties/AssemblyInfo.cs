92fe86326a4cd5c89f7c87bd32ab82a4cde500d9
bstriker <StrikeForceZero@gmail.com>
2016-03-16 02:29:37 -0400

    make com visible

--- pjsua2_net/Properties/AssemblyInfo.cs
+++ pjsua2_net/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("920fe31c-533e-44c8-acc1-3a4ee14b7360")]
